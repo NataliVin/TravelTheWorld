5bd0731afe35a181e3aaa7dac72ad09fb8dd7015
NataliVin <Natali_vin1@ukr.net>
2021-05-19 13:17:10 +0300

    commit1

---
+++ travelTheWorld/src/main/java/com/example/travelTheWorld/TravelTheWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelTheWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTheWorldApplication.class, args);
+	}
+
+}
